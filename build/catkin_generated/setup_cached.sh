2175407a8df8536759eb76f60ad4284ba274723b
Johann393 <johannhillermann393@gmail.com>
2025-09-01 00:31:53 -0300

    Primeiro commit - pacote Mapping

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/thomas/ros/projects/Mapping/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/thomas/ros/projects/Mapping/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/thomas/ros/projects/Mapping/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/thomas/ros/projects/Mapping/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/thomas/ros/projects/Mapping/src:$ROS_PACKAGE_PATH"
